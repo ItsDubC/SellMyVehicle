e3ecbd558d149c2d109c6754d4b121fba02bd10f
itsdubc <itsdubc@gmail.com>
2018-12-09 17:09:27 -0600

    Implemented Vehicle delete.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -90,5 +90,19 @@ namespace SellMyVehicle.Controllers
                 return Ok(result);
             }
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+                return NotFound();
+            
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+        }
     }
 }
