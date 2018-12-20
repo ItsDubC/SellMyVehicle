b83ca73c4eb8b4c8983f9babf28a0ad72af22ffa
ItsDubC <ItsDubC@gmail.com>
2018-12-20 06:56:42 -0600

    Adding Makes filter.

--- ClientApp/src/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/src/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { VehicleService } from 'src/app/services/vehicle.service';
 import { Vehicle } from 'src/app/models/vehicle';
+import { KeyValuePair } from 'src/app/models/KeyValuePair';
 //import { FormsModule } from "@angular/forms"
 
 @Component({
@@ -10,10 +11,15 @@ import { Vehicle } from 'src/app/models/vehicle';
 })
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
+  makes: KeyValuePair[];
 
   constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() {
+    this.vehicleService.getMakes().subscribe(m => {
+      this.makes = m;
+    });
+
     this.vehicleService.getVehicles().subscribe(v => {
       this.vehicles = v;
     })
